9a3d4d6f862ecc99d81c561f91ec52d3e3844982
Fernanda Reis Kennedy <fernandareisdp@gmail.com>
2023-03-11 18:10:29 -0500

    assigment done

---
+++ background.js
@@ -0,0 +1,16 @@
+//  //bonus
+//  function background(url, left, bottom, width, height){
+//     for (let h = 0; h < height; h++) {
+//         for (let w = 0; w < width; w++) {
+//             newImage(url, left + w*100, bottom + h*100);
+//         } 
+//     }
+// }
+
+// let horizon = window.innerHeight / 1.75
+// let heightofSky = window.innerHeight - horizon
+// let heightofGrass = horizon
+
+// //recalling bonus background function
+// background('assets/sky.png', 0, horizon, window.innerWidth/100, heightofSky/100)
+// background('assets/grass.png', 0, 0, window.innerWidth/100, heightofGrass/100)
